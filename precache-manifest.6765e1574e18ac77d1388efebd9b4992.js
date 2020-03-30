e93188a99fc855ceb66b652f1d2f1364aea42214
David Yudha <davidyudha@mail.ugm.ac.id>
2020-03-30 14:41:41 +0700

    Updates

---
+++ precache-manifest.6765e1574e18ac77d1388efebd9b4992.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d00d06ef8b8d3757f17416b98878482",
+    "url": "/Web3A-David-Yudha-W/index.html"
+  },
+  {
+    "revision": "54097db5e5b2370296f2",
+    "url": "/Web3A-David-Yudha-W/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "23fb19e9a6e6edaa74e7",
+    "url": "/Web3A-David-Yudha-W/static/css/main.837ec493.chunk.css"
+  },
+  {
+    "revision": "54097db5e5b2370296f2",
+    "url": "/Web3A-David-Yudha-W/static/js/2.9b15737b.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/Web3A-David-Yudha-W/static/js/2.9b15737b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23fb19e9a6e6edaa74e7",
+    "url": "/Web3A-David-Yudha-W/static/js/main.e7fed84f.chunk.js"
+  },
+  {
+    "revision": "9091ae5922d110d79c97",
+    "url": "/Web3A-David-Yudha-W/static/js/runtime-main.eca42986.js"
+  },
+  {
+    "revision": "51aeb3603c5bf611fe8f901be8fb64c1",
+    "url": "/Web3A-David-Yudha-W/static/media/harpot.51aeb360.svg"
+  }
+]);
